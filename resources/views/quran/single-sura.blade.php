d9a7cc511b45135fc64a8bd1455aa5a8945ab8b9
mohamedmansy316 <mohamedmansy855@gmail.com>
2023-07-04 20:38:38 +0300

    remove vite

--- resources/views/quran/single-sura.blade.php
+++ resources/views/quran/single-sura.blade.php
@@ -70,8 +70,8 @@
                             </div>
                         </div>
                         <!-- Modal -->
-                        <div class="modal fade" id="platformModal{{$Aya['id']}}" tabindex="-1" aria-labelledby="platform" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" data-bs-backdrop="static">
+                        <div class="modal fade" id="platformModal{{$Aya['id']}}" data-bs-backdrop="static" tabindex="-1" aria-labelledby="platform" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="modal-body d-flex flex-column justify-content-center text-center">
                                     <span class="social-platform facebook" data-platform="facebook" data-input="platformModal{{$Aya['id']}}" data-submit="#ModalSubmit{{$Aya['id']}}">Facebook</span>
