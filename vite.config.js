8bbbf52c5fa9250d986f3bb4405281c6df521053
mohamedmansy316 <mohamedmansy855@gmail.com>
2023-07-10 02:08:18 +0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: './'
+    },
     plugins: [
         laravel({
             input: [
-                ['resources/css/app.css', 'resources/js/app.js',' resources/js/custom.js'],
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/custom.js',
             ],
             refresh: true,
         }),
