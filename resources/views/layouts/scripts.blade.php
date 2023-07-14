d207c0bdb9235369061f8c8bb8b8a9c1527c6ff2
mohamedmansy316 <mohamedmansy855@gmail.com>
2023-07-14 15:04:36 +0300

    update styling

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,12 +1,11 @@
 {{-- PopperJs --}}
 <script src="{{url('public/js/popper.min.js')}}"></script>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-3dZ9wIrMMij8rOH7X3kLfXAzwtcHpuYpEgQg1OA4QAob1e81H8ntUQmQm3pBudqIoySO5j0tHN4ENzA6+n2r4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>Bootstrap Js --}}
 <script src="{{url('public/js/bootstrap.min.js')}}"></script>
 {{-- Datatable --}}
 <script src="{{url('public/js/datatables.min.js')}}"></script>
 {{-- Font Awesome --}}
 <script src="{{url('public/js/all.min.js')}}"></script>
 {{-- Js files --}}
-<script src="{{url('public/js/apps.js')}}"></script>
+<script src="{{url('/build/assets/app-1455c32c.js')}}"></script>
 <script type="module" src="{{url('public/js/custom.js')}}"></script>
 
