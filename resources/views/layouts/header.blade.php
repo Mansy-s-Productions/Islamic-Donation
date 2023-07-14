dfdb921bd7de494e7f3efa1a81895661c2d24e6a
mohamedmansy316 <mohamedmansy855@gmail.com>
2023-07-14 16:00:44 +0300

    change quran update route

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,6 +5,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>IslamHouse</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="تطوع لنشر آيات القرآن الكريم والأحاديث النبوية الشريفة بعدة لغات">
+        <meta name="keywords" content="القرآن الكريم , والأحاديث النبوية , تطوع , نشر , إسلام هاوس">
+        <link rel="canonical" href="https://volunteer.islamhouse.com/">
+
+        <title>مشاريع إسلام هاوس التطوعية</title>
+
+        <meta property="og:title" content="مشاريع إسلام هاوس التطوعية">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://volunteer.islamhouse.com/">
+        <meta property="og:image" content=" {{url('public/img/logo.png')}} ">
+        <meta property="og:description" content="تطوع لنشر القرآن الكريم والأحاديث النبوية الشريفة بعدة لغات">
+        <meta property="og:site_name" content="Naqrah ﻧﻘﺮﺓ">
+        <meta name="twitter:card" content="تطوع لنشر القرآن الكريم والأحاديث النبوية الشريفة بعدة لغات">
+        <meta name="twitter:image" content="{{url('public/img/logo.png')}}">
+        <meta name="twitter:title" content="مشاريع إسلام هاوس التطوعية" />
+        <meta name="twitter:description" content="تطوع لنشر القرآن الكريم والأحاديث النبوية الشريفة بعدة لغات">
+        <meta name="application-name" content="إسلام هاوس">
+        <meta name="msapplication-TileImage" content="{{url('public/img/logo.png')}}">
+        <meta name="msapplication-TileColor" content="#316eec">
 
         <link rel="icon" href="{{url('public/img/logo.png')}}">
         <!-- Bootstrap -->
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,12 +1,12 @@
 {{-- PopperJs --}}
-<script src="{{url('public/js/popper.min.js')}}"></script>
-<script src="{{url('public/js/bootstrap.min.js')}}"></script>
+<script src="{{url('public/js/popper.min.js')}}" defer></script>
+<script src="{{url('public/js/bootstrap.min.js')}}" async></script>
 {{-- Datatable --}}
-<script src="{{url('public/js/datatables.min.js')}}"></script>
+<script src="{{url('public/js/datatables.min.js')}}" defer></script>
 {{-- Font Awesome --}}
-<script src="{{url('public/js/all.min.js')}}"></script>
+<script src="{{url('public/js/all.min.js')}}" async></script>
 {{-- Js files --}}
-<script src="{{url('public/js/apps.js')}}"></script>
-<script src="{{url('/build/assets/app-1455c32c.js')}}"></script>
-<script type="module" src="{{url('public/js/custom.js')}}"></script>
+<script src="{{url('public/js/apps.js')}}" defer></script>
+<script src="{{url('/build/assets/app-1455c32c.js')}}" defer></script>
+<script type="module" src="{{url('public/js/custom.js')}}" defer></script>
 
