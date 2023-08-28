97cafa461e78bbad8ad0e456b157f5131c8858d9
faniabdo99 <faniabdo99@gmail.com>
2023-08-28 20:37:24 +0300

    Minor footer updates

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,14 +7,11 @@
                             width="150" alt="islamhouse.com"></a>
                 </div>
                 <div class="col-lg-4 col-md-4 col-12 mb-4 mb-md-6">
-                    <h3 class="fw-medium font-sans-serif text-secondary mb-3">روابط هامة</h3>
+                    <h3 class="fw-medium font-sans-serif text-secondary mb-3">روابط دعوية هامة</h3>
                     <div class="d-flex flex-column">
-                        <a class="d-flex my-1" href="https://hadeethenc.com/ar" target="_blank">موسوعة الأحاديث النبوية</a>
-                        <a class="d-flex my-1" href="https://quranenc.com/ar" target="_blank">موسوعة القرآن الكريم</a>
+                        <a class="d-flex my-1"  href="{{route('islamicLinks')}}" target="_blank">موسوعة بيان الإسلام </a>
                         <a class="d-flex my-1" href="https://kids.islamenc.com/ar/home/" target="_blank">منصة ما لا يسع أطفال المسلمين جهله</a>
-                        <a class="d-flex my-1" href="https://saadi.islamenc.com/ar/browse/tafsir/saadi/" target="_blank">منصة تفسير السعدي</a>
                         <a class="d-flex my-1" href="https://mofeed.org" target="_blank">موقع مفيد</a>
-                        <a class="d-flex my-1" href="https://islamenc.com/index.php/ar/home" target="_blank">باحث الموسوعات</a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-12 mb-4 mb-md-6">
